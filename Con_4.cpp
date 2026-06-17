54f18eed9814f268de5d54f86d8a096a521bd086
uvrajMhto19 <yuvrajkumarkumar283@gmail.com>
2026-06-17 15:36:18 +0530

    Add files via upload

---
+++ Con_4.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int days;
+    cin>>days;
+    
+    switch(days){
+        case1:
+        cout<<"Monday"<<endl;
+        break;
+
+        case2:
+        cout<<"Tuesday"<<endl;
+        break;
+
+        case3:
+        cout<<"Wednesday"<<endl;
+        break;
+
+        default:
+        cout<<"default case"<<endl;
+    }
+    return 0;
+}
